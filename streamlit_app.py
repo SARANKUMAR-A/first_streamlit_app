6529a07a47b0510ccb8dcafdd84931aeb013bdcc
SARANKUMAR-A <116876312+SARANKUMAR-A@users.noreply.github.com>
2022-10-28 15:40:53 +0530

    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header(🥣 'Breakfast Menu')
-streamlit.text(🥗'Onega 3 & Blueberry Ostneal')
-streamlit.text(🐔 'Kale, Spinach & Rocket Smocthio')
-streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
+streamlit.header( 'Breakfast Menu')
+streamlit.text(🥣'Onega 3 & Blueberry Ostneal')
+streamlit.text(🥗 'Kale, Spinach & Rocket Smocthio')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞'Avocado Toast')
+
 
    
