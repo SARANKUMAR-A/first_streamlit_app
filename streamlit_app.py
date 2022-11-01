370e7b2d559b5a06d13ea717f83dff734eb14881
SARANKUMAR-A <116876312+SARANKUMAR-A@users.noreply.github.com>
2022-11-01 15:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,30 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
-
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header( 'Breakfast Menu')
-streamlit.text('🥣Onega 3 & Blueberry Ostneal')
-streamlit.text('🥗Kale, Spinach & Rocket Smocthio')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado' , 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page
-streamlit.dataframe(fruits_to_show)
-
-#create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-#New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice: 
-  streamlit.error("Please select a fruit to get information.")
-else:
-  back_from_function = get_fruityvice_data(fruit_choice)
-  streamlit.dataframe(back_from_function)
-
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+create table MAX_OUTGOING.FOR_ACME.LOTSTOCKRETURN as
+SELECT 
+l.VIN
+,y.MODYEARNAME
+,m.MAKE_NAME
+,v.DESC1
+,v.DESC2
+,v.DESC3
+,BODYSTYLE
+,ENGINE
+,DRIVETYPE
+,TRANS
+,MPG
+,MANUF_NAME
+,COUNTRY
+,VEHICLETYPE
+,PLANTNAME
+FROM ACME_DETROIT.ADU.LOTSTOCK l -- he joins Lottie's data from the INBOUND SHARE 
+JOIN MAX_VIN.DECODE.WMITOMANUF w -- with all his data (he just tested)
+    ON l.WMI=w.WMI
+JOIN MAX_VIN.DECODE.MANUFTOMAKE m
+    ON w.manuf_id=m.manuf_id
+JOIN MAX_VIN.DECODE.MANUFPLANTS p
+    ON l.plantcode=p.plantcode
+    AND m.make_id=p.make_id
+JOIN MAX_VIN.DECODE.MMVDS v
+    ON v.vds=l.vds 
+    and v.make_id = m.make_id
+JOIN MAX_VIN.DECODE.MODELYEAR y
+    ON l.modyearcode=y.modyearcode;
